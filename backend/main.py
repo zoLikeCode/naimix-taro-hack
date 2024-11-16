05e5f3ebd6a35ab4e07e99fb35baaf70bcc63b63
Nikita Zontov <zontovnikita@gmail.com>
2024-11-17 01:30:25 +0300

    New upd

--- backend/main.py
+++ backend/main.py
@@ -72,6 +72,6 @@ def pdf_reader(path_to_pdf):
     reader = PdfReader(path_to_pdf)
     text = ""
     for page in reader.pages:
-        text += page.extract_text()
+        text += page.extract_text() 
     return text
     
